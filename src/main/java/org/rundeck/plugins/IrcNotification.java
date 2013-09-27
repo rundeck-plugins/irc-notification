276ce28ce16cd231cec4d9b3e96d353426699bd5
Alex Honor <alexhonor@yahoo.com>
2013-09-26 18:06:07 -0700

    initial commit

---
+++ src/main/java/org/rundeck/plugins/IrcNotification.java
@@ -0,0 +1,79 @@
+package org.rundeck.plugins;
+
+import com.dtolabs.rundeck.core.plugins.Plugin;
+import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
+import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
+import com.dtolabs.rundeck.plugins.notification.NotificationPlugin;
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.NickAlreadyInUseException;
+import org.jibble.pircbot.PircBot;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+@Plugin(service = "Notification", name = "IRC")
+@PluginDescription(title = "IRC Notification Plugin", description = "Send job notifications to an IRC channel.")
+public class IrcNotification implements NotificationPlugin {
+
+    @PluginProperty(name = "server", title = "Server host", description = "Chat server (eg, irc.freenode.net)")
+    private String server;
+
+    @PluginProperty(name = "channel", title = "IRC channel", description = "The IRC channel to join")
+    private String channel;
+
+    public IrcNotification() {
+
+    }
+
+    @Override
+    public boolean postNotification(String trigger, Map executionData, Map config) {
+        System.err.printf("Trigger %s fired for %s, configuration: %s\n", trigger, executionData, config);
+        System.err.printf("Channel is: %s\n", channel);
+
+        IrcBot bot = new IrcBot();
+        bot.setVerbose(true);
+        try {
+
+            bot.connect(server);
+            bot.setAutoNickChange(true);
+            bot.joinChannel(channel);
+            bot.sendPrivMessage(trigger, executionData);
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            return false;
+        } finally {
+            bot.disconnect();
+        }
+
+        return true;
+    }
+
+    private String generateMessage(String trigger, Map executionData) {
+        Object job = executionData.get("job");
+        Map jobdata = (Map) job;
+        Object groupPath = jobdata.get("group");
+        Object jobname = jobdata.get("name");
+        String jobdesc = (!isBlank(groupPath.toString()) ? groupPath + "/" : "") + jobname;
+
+        return "[" + trigger.toUpperCase() + "] " + jobdesc + " run by " + executionData.get("user") + ": " +
+                executionData.get("href");
+    }
+
+    private boolean isBlank(String string) {
+        return null == string || "".equals(string);
+    }
+
+    public class IrcBot extends PircBot {
+        public IrcBot() {
+            setName("RundeckBot");
+        }
+
+        void sendPrivMessage(String trigger, Map executionData) {
+            sendRawLine("PRIVMSG " + channel + " :" + generateMessage(trigger, executionData));
+        }
+
+    }
+
+}
